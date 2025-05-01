7b56838a1257bd7b9433fac5f7ebe74fc9020a8f
Lois Gonzales <loisjersong@gmail.com>
2025-05-02 01:27:10 +0800

    feat: update readme

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    exclude: ['e2e', 'e2e/**', 'node_modules', 'dist'],
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./tests/setup.ts'],
