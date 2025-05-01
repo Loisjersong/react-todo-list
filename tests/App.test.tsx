bb9819644e9780873a88eb74e445fb9908e4c376
Lois Gonzales <loisjersong@gmail.com>
2025-05-01 18:06:55 +0800

    feat: setup vitest and tailwindcss

---
+++ tests/App.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import App from '../src/App'
+
+describe('App', () => {
+  it('should render heading', () => {
+    render(<App />)
+    const heading = screen.getByRole('heading')
+    expect(heading).toBeInTheDocument()
+
+    expect(heading).toHaveTextContent(/react starter/i)
+  })
+})
